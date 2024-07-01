994f3ddc4af1db5618249e0a7cd18c3b38cb1c2d
kozloff-spec <63442864+kozloff-spec@users.noreply.github.com>
2024-07-01 13:52:51 +0300

    78
    hfgfjh

---
+++ js.js
@@ -0,0 +1,4 @@
+function addToCart(productName, productPrice) {
+    alert(`${productName} has been added to your cart for $${productPrice}`);
+    // Implement further functionality for adding to cart if needed
+}
