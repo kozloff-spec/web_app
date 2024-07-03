2d20636b0a96d78c8ccad838f263fdc58d199090
kozloff-spec <63442864+kozloff-spec@users.noreply.github.com>
2024-07-04 00:47:22 +0300

    Update js.js

--- js.js
+++ js.js
@@ -77,8 +77,8 @@ let usercard = document.getElementById('usercard');
 
 let p = document.createElement("p");
 
-p.innerText = `${tg.initDataUnsafe.first_name}
-${tg.initDataUnsafe.last_name}
+p.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}
 ${tg.initDataUnsafe.user.id} `
 
 usercard.appendChild(p)
